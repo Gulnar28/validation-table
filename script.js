e73a7dd3c8ebb2f2bf8a5b5c0e2d94b266a14760
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-06-10 15:08:12 +0400

    first

--- script.js
+++ script.js
@@ -5,27 +5,33 @@ let color = '';
 
 let slc = document.getElementById('slc');
 
-slc.addEventListener('change', ()=>{
-    console.log('value changed');
-})
+slc.addEventListener('change',render)
     
 
 
 
 
+function render() {
     let select = parseInt(slc.value);
- for (let i = 0; i < select; i++) {
-  data += `<tr>`;
-
-  for (let j = 0; j < select; j++) {
-    color = (i+j)%2 === 0 ? 'black' : '';
-    data += `<td style="background-color:${color};">  </td>`;
-   }
-
-  data += `</tr>`;
- }
+    data = ''
+        console.log(select)
+        for (let i = 0; i < select; i++) {
+            data += `<tr>`;
+          
+            for (let j = 0; j < select; j++) {
+              color = (i+j)%2 === 0 ? 'black' : '';
+              data += `<td style="background-color:${color};">  </td>`;
+             }
+          
+            data += `</tr>`;
+           }
+        console.log(data);
 
 document.getElementById('tbl').innerHTML = data;
+        
+    }
+ select.addEventListener("change",render)
+
 
 
 
@@ -37,23 +43,23 @@ document.getElementById('tbl').innerHTML = data;
 
     
  
-//     form.addEventListener('submit',(e) => {
+    // form.addEventListener('submit',(e) => {
  
-//         e.preventDefault()
+    //     e.preventDefault()
         
 
     
-//         const email = document.getElementById('email').value;
-//         const password = document.getElementById('password').value;
+    //     const email = document.getElementById('email').value;
+    //     const password = document.getElementById('password').value;
       
        
-//         if(email === "email" && password === "password"){
-//             alert("Correct")
-//         }
-//         else{
-//            alert("Incorrect")
-//         }
-//     })
+    //     if(email === email && password === password){
+    //         alert("Correct")
+    //     }
+    //     else{
+    //        alert("Incorrect")
+    //     }
+    // })
 
    
     
