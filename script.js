b3ddcbd5f8dc8c28b0a653e18ab4a29e35e20192
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-06-18 14:42:50 +0400

    first

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
 "use strict";
 
-let data = "";
-let color = '';
+// let data = "";
+// let color = '';
 
-let slc = document.getElementById('slc');
+// let slc = document.getElementById('slc');
 
-slc.addEventListener('change',render)
+// slc.addEventListener('change',render)
     
 
 
 
 
-function render() {
-    let select = parseInt(slc.value);
-    data = ''
-        console.log(select)
-        for (let i = 0; i < select; i++) {
-            data += `<tr>`;
+// function render() {
+//     let select = parseInt(slc.value);
+//     data = ''
+//         console.log(select)
+//         for (let i = 0; i < select; i++) {
+//             data += `<tr>`;
           
-            for (let j = 0; j < select; j++) {
-              color = (i+j)%2 === 0 ? 'black' : '';
-              data += `<td style="background-color:${color};">  </td>`;
-             }
+//             for (let j = 0; j < select; j++) {
+//               color = (i+j)%2 === 0 ? 'black' : '';
+//               data += `<td style="background-color:${color};">  </td>`;
+//              }
           
-            data += `</tr>`;
-           }
-        console.log(data);
+//             data += `</tr>`;
+//            }
+//         console.log(data);
 
-document.getElementById('tbl').innerHTML = data;
+// document.getElementById('tbl').innerHTML = data;
         
-    }
- select.addEventListener("change",render)
+//     }
+//  select.addEventListener("change",render)
 
 
 
 
-// let email = 'admin@gmail.com';
-// let password = '12345';
-
-// const submitButton = document.getElementById('button');
 
 
+
+   const submitButton = document.getElementById('button');
+
+
+   form.addEventListener('submit',(e) => {
+
+       e.preventDefault()
     
- 
-    // form.addEventListener('submit',(e) => {
- 
-    //     e.preventDefault()
+
+    function Input(){
+
+    let email = 'admin@gmail.com';
+    let pass = '12345';
+
+    const userEmail = document.getElementById('email').value;
+    const userPassword = document.getElementById('password').value;
+
+    if(email===userEmail && pass===userPassword) {
+
+        alert("You've successfully logged in")
         
-
-    
-    //     const email = document.getElementById('email').value;
-    //     const password = document.getElementById('password').value;
-      
-       
-    //     if(email === email && password === password){
-    //         alert("Correct")
-    //     }
-    //     else{
-    //        alert("Incorrect")
-    //     }
-    // })
+    }else{ alert("Username or password is incorrect")}
 
    
-    
+       
+}
+   Input();
+})
+   
+
+
+   
